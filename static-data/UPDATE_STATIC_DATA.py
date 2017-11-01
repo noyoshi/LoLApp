0552d7472869c6b6f98baa854528d92375d6ae30
silver <noahcy117@gmail.com>
2017-11-01 13:35:25 -0400

    added comments describing how to use retrieved json data

--- static-data/UPDATE_STATIC_DATA.py
+++ static-data/UPDATE_STATIC_DATA.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2.7
+
 # -*- coding: utf-8 -*-
 
 # This will update the static database, with the most recent codes for
@@ -13,11 +15,32 @@ headers = {
     "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
     "X-Riot-Token": API_KEY,
     "Accept-Language": "en-US,en;q=0.8",
-    "tags": "all"
+    "tags": "spells"
+    # "dataById" : "false"
 }
-
+#WARNING: THIS REQUEST IS LIMITED TO 10/HR WITH THE DEV KEY
 region = "na1"
-ext = "lol/static-data/v3/champions"
+ext = "lol/static-data/v3/champions?locale=en_US&tags=spells&dataById=false"
 url = "https://{region}.api.riotgames.com/{ext}".format(region=region, ext=ext)
 data = requests.get(url, headers=headers)
 print data.json()
+
+#IMPORTANT: data.json() is a dictionary with keys mapping to the names of the champions
+
+# spell_dict = data.json()
+# katarina = spell_dict["Katarina"]
+# in katarina:
+# spells, title, id, key, name
+# spells, title, name return a unicode string
+# .encode("utf-8") fixes this ^
+
+# test_url = "https://na1.api.riotgames.com/lol/static-data/v3/champions/44?locale=en_US&tags=spells"
+# test_data = requests.get(test_url)
+# print test_data
+# region = "na1"
+# ext = "lol/static-data/v3/champions/44?locale=en_US&tags=spells"
+# url = "https://{region}.api.riotgames.com/{ext}".format(region=region, ext=ext)
+# data = requests.get(url, headers=headers)
+# print data.json()
+
+# THis shit up here works... dont fuck with the headers
